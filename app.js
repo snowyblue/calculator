0436166c7f3087a968f321a7cf906a6ed1b5b970
Amie Wei <amie.wei12@gmail.com>
2022-11-12 16:32:31 -0600

    updated logic

--- app.js
+++ app.js
@@ -1,47 +1,115 @@
 const btnNums = document.querySelectorAll('.btnNum');
-const btnPlus = document.querySelector('.btnPlus');
+const btnOperator = document.querySelectorAll('.btnOperator');
+const btnEqual = document.querySelector('.btnEqual');
 const btnClear = document.querySelector('.btnClear');
 const btnDelete = document.querySelector('.btnDelete');
+let display = document.querySelector('.outcome')
 
+let aNum = ""
+let operator = ""
+let bNum = ""
 
 // gets current num by concating the nums together
-const store = function(num) {
-    let currentNum = ""
-    let selectedNum = num.target.innerText
-    currentNum
-    console.log(currentNum)
+function firstNumber(e) {
+    console.log(`operator sign: ${operator}`)
+    if (operator === "=") {
+        clear()
+    }
+    if (operator === "") {
+        console.log('1stNumber - 1st function')
+        let selectedNum = e.target.innerText
+        aNum+=selectedNum
+        console.log(aNum)
+        // display number on screen
+        display.innerText = aNum
+    } 
+
 }
 
-const add = function(a,b) {
+function secondNumber(e) {
+    console.log(`operator sign: ${operator}`)
+    if (operator !== "" && operator !== "=") {
+        console.log('2ndNumber - 2st function' + e.target.innerText)
+        let selected2Num = e.target.innerText
+        bNum+=selected2Num
+        console.log(bNum)
+        // display number on screen
+        display.innerText = bNum
+    }
+}
+
+function getOperator(e) {
+    continueCalc = false
+    console.log('getOperator function'+continueCalc)
+    operator = e.target.innerText
+    console.log(operator)
+    btnNums.forEach(num => num.addEventListener('mousedown', secondNumber))
+}
+
+function operate(a, b, operator) {
+    a = Number(a);
+    b = Number(b);
+    console.log(`operate is on: a:${a} b:${b} operator:${operator}`)
+    if (operator === "÷" && b === 0) {
+        clear()
+        display.innerText = 'sneaky!'
+        // why this doesn't clear the operator sign? when another number is pressed? bottom should be same as clear
+        //why both first and second number functions are triggered at the same time?
+    } else {
+        if (operator === "+") {
+            result = add(a,b)
+        } else if (operator === "-") {
+            result = subtract(a,b)
+        } else if (operator === "x") {
+            result = multiply(a,b) 
+        } else if (operator === "÷") {
+            result = divide(a,b) 
+        } else if (operator === "") {
+            result = a
+        }
+        display.innerText = Math.round(result*100)/100
+        aNum = result
+        console.log(`elseif running, result: ${result}`)
+    }
+    bNum = ""
+    operator = ""
+    console.log(`outcome: ${aNum}, ${bNum}, ${operator}`)
+}
+
+function add(a,b) {
+    console.log(a+b)
     return a+b
-  };
-  
-const subtract = function(a,b) {
+};
+
+function subtract(a,b) {
+    console.log(a-b)
     return a-b
 };
   
-const sum = function(arr) {
-    return arr.reduce((total, num) => num + total, 0)
+function multiply(a,b) {
+    console.log(a*b)
+    return a*b
 };
 
-// console.log(sum([1,3,5]))
-
-const multiply = function(arr) {
-    return arr.reduce((product, num) => num*product, 1)
-};
-// console.log(multiply([1,3,5]))
-
-const power = function(a, b) {
-    return a**b
+function divide(a,b) {
+    console.log(a/b)
+    return a/b
 };
 
-const factorial = function(a) {
-    let fac = 1
-        for (let i = 1; i <=a; i++) {
-        fac *= i
-    }
-return fac
+function clear() {
+    aNum = ""
+    bNum = ""
+    operator = ""
+
+    // console.log(display)
+    display.innerText = ""
 };
 
-btnNums.forEach(num => num.addEventListener('mousedown', store))
-console.log(btnNums)
+btnNums.forEach(num => num.addEventListener('mousedown', firstNumber))
+
+btnOperator.forEach(operator => operator.addEventListener('click', getOperator))
+
+btnEqual.addEventListener('mousedown', () => operate(aNum,bNum,operator))
+
+btnClear.addEventListener('mousedown', clear)
+// console.log(btnNums)
